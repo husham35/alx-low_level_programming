921725abfde516c32ecc021bb3aa631b80e9833f
Hamza Husein <husham35@gmail.com>
2023-06-15 13:10:12 -0700

    Corrected 4-puts.c to betty standard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 /**
- * Main - Entry poing
+ * main - Entry poing
  *
- * Always return (0)
+ * Return: always 0 (success)
  */
 
-
- int main(void)
- {
- 
- 	puts("\"Programming is like building a multilingual puzzle");
-	
- 	return (0);
- 
- }
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
