6e7d3b5843c2d097620755c294064dda69a7cff6
Hamza Husein <husham35@gmail.com>
2023-06-16 09:45:18 -0700

    print alphabet from a-z excluding q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: This program prints the
+ * the alphabets from a-Z excluding e and q.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet == 'e' || alphabet == 'q')
+			continue;
+		else
+			putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
