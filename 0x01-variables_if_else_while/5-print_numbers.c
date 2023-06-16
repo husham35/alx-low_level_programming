9a2a775dbdf893b1c315b29fa5eb5bc2bab5d5ec
Hamza Husein <husham35@gmail.com>
2023-06-16 10:13:13 -0700

    numbers from 0-9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - main block
  * Description: This program prints the
- * the single numbers from 1 to 10.
+ * the single numbers from 1 to 9.
  * Return: 0
  */
 
@@ -11,7 +11,7 @@ int main(void)
 {
 	int number = 0;
 
-	while (number <= 10)
+	while (number <= 9)
 	{
 		printf("%d", number);
 		number++;
