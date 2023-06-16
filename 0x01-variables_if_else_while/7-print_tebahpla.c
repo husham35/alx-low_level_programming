7ec3576e879f1013eac3293c7eb9d42a74e61055
Hamza Husein <husham35@gmail.com>
2023-06-16 10:24:26 -0700

    alphabet z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: This program prints the
+ * the alphabets from z-a.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	printf("\n");
+
+	return (0);
+}
