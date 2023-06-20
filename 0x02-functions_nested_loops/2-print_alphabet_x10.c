3391718a1cd4f47864efb3d607c7e6f17ffc6ff3
Hamza Husein <husham35@gmail.com>
2023-06-20 10:50:18 -0700

    Print a-z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,16 @@
 void print_alphabet_x10(void)
 {
 	int i = 0;
-	char a = 'a';
-	
 
 	while (i < 10)
 	{
+		char a = 'a';
 		while (a <= 'z')
         	{
 			 _putchar(a);
 			 a++;
 		}
-	_putchar('\n');
-	i++;
+		_putchar('\n');
+		i++;
 	}
 }
