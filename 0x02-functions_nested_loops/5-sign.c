056f7ad2f74918db173a57e657c8edac5d17315b
Hamza Husein <husham35@gmail.com>
2023-06-20 12:42:02 -0700

    Print the sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - returns 1 if n > 0
+ *  return 0 if n = 0
+ *  return -1 if n < 0
+ * @n: number to check
+ * Return: 1 if n > 0, 0 if n = 0 and -1 if n < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0)
+	else
+		return (-1);
+}
