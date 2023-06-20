e84ed806997caf155050456ec67c33403d4364e5
Hamza Husein <husham35@gmail.com>
2023-06-20 14:32:16 -0700

    Integer addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integer type numbers
+ * @x: int type number
+ * @y: int type number
+ * Return: sum of x and y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
