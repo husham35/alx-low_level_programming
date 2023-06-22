481ccb2c8b561d5b9d483c171d14a60242970f91
Hamza Husein <husham35@gmail.com>
2023-06-22 07:16:05 -0700

    corrected check if character is upper case or not

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - checks if a character is uppercase or not.
- *
+ *@c: character to check
  * Return: 1 if uppercase and 0 if not
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <='Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
