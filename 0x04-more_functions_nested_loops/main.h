cab8573b17e3f0540b8a495d3d9bf02ef51891e2
Hamza Husein <husham35@gmail.com>
2023-06-22 07:10:58 -0700

    check if character is upper case or not

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * _isupper - checks if a character is upper case or not
+ * @c: parameter to check
+ * Return: 1 if uppercase or 0 if not
+ */
+int _isupper(int c);
+
+
+#endif
