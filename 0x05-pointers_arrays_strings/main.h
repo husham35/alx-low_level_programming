28fca082ab8557326f93d962ba9b0823793a9edb
Hamza Husein <husham35@gmail.com>
2023-06-26 13:27:07 -0700

    Length of string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -77,14 +77,12 @@ void print_triangle(int size);
  */
 void print_number(int n);
 
-
 /**
  * reset_to_98 - resets value of integer type number
  * @n: pointer integer
  * Return: void
  */
 void reset_to_98(int *n);
-
 
 /**
  * swap_int - swaps integer type numbers
@@ -93,6 +91,68 @@ void reset_to_98(int *n);
  * Return: void
  */
 void swap_int(int *a, int *b);
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer char
+ * Return: void
+ */
+int _strlen(char *s);
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: pointer char
+ * Return: void
+ */
+void _puts(char *str);
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: pointer char
+ * Return: void
+ */
+void print_rev(char *s);
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer char
+ * Return: void
+ */
+void rev_string(char *s);
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: pointer char
+ * Return: void
+ */
+void puts2(char *str);
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: pointer char
+ * Return: void
+ */
+void puts_half(char *str);
+
+/**
+ * print_array - prints n elements of an array of integers, followed by a new line
+ * @a: pointer integer
+ * @n: integer
+ * Return: void
+ */
+void print_array(int *a, int n);
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: pointer char
+ * @src: pointer char
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src);
+
+
 
 
 
