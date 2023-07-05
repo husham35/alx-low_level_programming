dd9574cce8374bc5f923bf1929134d7c4f523fb5
Hamza Husein <husham35@gmail.com>
2023-07-05 10:20:42 -0700

    corrected palindrome code

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,47 @@
 #include "main.h"
-#include <string.h>
+
+/**
+ * compare_char - compare head and tail indices for match
+ * @h: index starting from left hand side of string
+ * @t: index starting from right hand side of string
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int compare_char(char *h, char *t)
+{
+
+	if (h >= t)
+		return (1);
+	if (*h == *t)
+		return (compare_char(h + 1, t - 1));
+
+	return (0);
+}
+
+/**
+ * string_length - find length of string to access last char index
+ * @s: string
+ * Return: length
+ */
+
+int string_length(char *s)
+{
+
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + (string_length(s)));
+}
 
 /**
  * is_palindrome - check if palindrome
  * @s: string to check
- * Return: 1 if palindrome, 0 if otherwise
+ * Return: 1 if palindrome, 0 if not
  */
 
 int is_palindrome(char *s)
 {
-	int length = strlen(s);
-	int i, j;
+	int length = string_length(s);
 
-	for (i = 0, j = length - 1; i < j; i++, j--)
-	{
-		if (s[i] != s[j])
-		{
-			return (0); /* Not a palindrome */
-		}
-	}
-
-	return (1); /* palindrome */
+	return (compare_char(s, (s + length - 1)));
 }
