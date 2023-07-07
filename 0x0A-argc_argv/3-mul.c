e631d9e9d0698563b867a4801b1d4052ca15298e
Hamza Husein <husham35@gmail.com>
2023-07-07 13:18:29 -0700

    multiply arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: prints product of argument numbers
+ * @argc: argument counter
+ * @argv: numbers to multiply
+ * Return: 0 on success, 1 if two arguments not given
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
