ea1db9158a13cea3fea1429ffe64e5fbd4bc8349
Hamza Husein <husham35@gmail.com>
2023-07-20 17:48:29 -0700

    sum all parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of parameters
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+	int sum;
+
+	va_start(list, n);
+	for (i = 0, sum = 0; i < n; i++)
+		sum = sum + va_arg(list, int);
+
+	va_end(list);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,21 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+void print_char(va_list list);
+void print_str(va_list list);
+void print_float(va_list list);
+void print_int(va_list list);
+
+typedef struct check_list
+{
+	char *type;
+	void (*f)();
+} check_list;
+
+#endif
