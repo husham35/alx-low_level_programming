718c2ca30ebfe8db4cf75a2938b91c8d9a2d9854
Hamza Husein <husham35@gmail.com>
2023-07-20 18:41:16 -0700

    corrected sum them all function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,13 +12,15 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-	unsigned int sum = 0;
+	int sum;
 
 	if (n == 0)
 		return (0);
 	va_start(list, n);
+	sum = 0;
 	for (i = 0; i < n; i++)
-		sum = sum + va_arg(list, int);
+		sum += va_arg(list, int);
+
 	va_end(list);
 
 	return (sum);
