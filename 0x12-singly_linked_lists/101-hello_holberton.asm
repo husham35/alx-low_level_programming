d6e62fc9ef4ed0dc82639837906d46f50d0e86d0
Hamza Husein <husham35@gmail.com>
2023-07-27 09:49:41 -0700

    print Hello, Holberton in assembly language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,13 @@
 section .data
-    message db 'Hello, Holberton', 10 ; note the newline at the end
+    format db "Hello, Holberton", 10, 0 ; format string with newline character
 section .text
-    global _start
-_start:
-    ; write the message to stdout
-    mov eax, 4 ; system call for write
-    mov ebx, 1 ; file handle 1 is stdout
-    mov ecx, message ; address of string to output
-    mov edx, 16 ; number of bytes to output
-    int 0x80 ; invoke operating system to do the write
-
-    ; exit the program
-    mov eax, 1 ; system call for exit
-    xor ebx, ebx ; exit code 0
-    int 0x80 ; invoke operating system to exit	
+    extern printf
+    global main
+main:
+    sub rsp, 8 ; align stack
+    mov edi, format ; load the address of the format string
+    xor eax, eax ; clear eax register
+    call printf ; call the printf function
+    add rsp, 8 ; restore stack
+    mov eax, 0 ; return 0
+    ret	
