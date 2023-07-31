c35e94eab411c2f930184605e11c2fbd11e3fb0b
Hamza Husein <husham35@gmail.com>
2023-07-31 10:36:56 -0700

    Add new node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,39 +1,25 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index of a listint_t list
+ * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: pointer to head pointer of linked list
- * @index: index of node to delete starting at 0
- * Return: 1 if success, -1 if failed
+ * @n: data to add to new node
+ * Return: address of new element, or NULL if failed
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *current;
-	listint_t *h;
-	unsigned int i;
 
-	i = 0;
-	current = *head;
-	if (*head == NULL)
-		return (-1);
+	listint_t *new_node;
 
-	while (i < index)
-	{
-		i++;
-		h = current;
-		if (current->next)
-			current = current->next;
-		else
-			return (-1);
-	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-	if (index == 0)
-		*head = current->next;
-	else if (current->next)
-		h->next = current->next;
-	else
-		h->next = NULL;
+	new_node->n = n;
 
-	free(current);
-	return (1);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
