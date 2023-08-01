6900dfd56c15bfc403cf4babba82ee832b8d6390
Hamza Husein <husham35@gmail.com>
2023-08-01 10:27:23 -0700

    print linked list with addresses

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list with addresses
+ * @head: head of linked list
+ * Description: go through the list only once.
+ * Return: number of nodes in list, exit(98) if failed
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current;
+	size_t count;
+	const listint_t *hold;
+
+	current = head;
+	if (current == NULL)
+		exit(98);
+
+	count = 0;
+	while (current != NULL)
+	{
+		hold = current;
+		current = current->next;
+		count++;
+		printf("[%p] %d\n", (void *)hold, hold->n);
+
+		if (hold < current)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
+		}
+	}
+
+	return (count);
+}
