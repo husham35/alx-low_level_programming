bdba81224fe0c40367c8211c1e4789f99997747b
Hamza Husein <husham35@gmail.com>
2023-08-03 08:24:58 -0700

    correction to binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,25 +8,26 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int length = 0, power = 1;
-	unsigned int sum = 0, error = 0;
+
+	int length = 0, i;
+	unsigned int sum = 0;
 
 	if (b == NULL)
-		return (error);
+		return (sum);
 
 	while (b[length] != '\0')
 		length++;
 	length -= 1;
 
-	while (length >= 0)
+	i = 0;
+	while (b[i])
 	{
-		if ((b[length] != '0') && (b[length] != '1'))
-			return (error);
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (sum);
 
-		if (b[length] == '1')
-			sum += power;
-
-		power = 2;
+		if (b[i] == '1')
+			sum += (1 * (1 << length));
+		i++;
 		length--;
 	}
 
