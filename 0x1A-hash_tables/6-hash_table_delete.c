990ebb5e54c2193596611a3527645a173e7042d6
husham35 <husham35@gmail.com>
2024-02-02 15:47:09 +0000

    corrected task 7 again

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,7 +7,7 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *next;
+	hash_node_t *next_node;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return;
@@ -15,11 +15,11 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		while (ht->array[i] != NULL)
 		{
-			next = ht->array[i]->next;
+			next_node = ht->array[i]->next;
 			free(ht->array[i]->key);
 			free(ht->array[i]->value);
 			free(ht->array[i]);
-			ht->array[i] = next;
+			ht->array[i] = next_node;
 		}
 	}
 	free(ht->array);
